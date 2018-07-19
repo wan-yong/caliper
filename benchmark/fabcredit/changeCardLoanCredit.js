33308e6f232f739d81fcd4ac748d5bf31349bcb9
Yong Wan <wan_yong@msn.com>
2018-07-19 17:46:09 +0800

    根据要求，修改了config.json

--- benchmark/fabcredit/changeCardLoanCredit.js
+++ benchmark/fabcredit/changeCardLoanCredit.js
@@ -1,19 +1,19 @@
 /**
+* Changed from 
 * Copyright 2017 HUAWEI. All Rights Reserved.
-*
+* Author: Yong Wan
 * SPDX-License-Identifier: Apache-2.0
-*
-*/
+**/
 
 
 'use strict';
 
 const crypto = require('crypto');
 
-module.exports.info  = 'publishing digital items';
+module.exports.info  = 'change the Credit Card Loan'; //修改
 
 let bc, contx;
-let itemBytes = 1024;   // default value
+let itemBytes = 1024;   // 修改，对应 config.json的"arguments": {  "itemBytes": 2048000 }
 let ids = [];           // save the generated item ids
 
 module.exports.ids = ids;
@@ -39,7 +39,7 @@ module.exports.run = function() {
         'info' : '',
         'item' : buf
     };
-    return bc.invokeSmartContract(contx, 'drm', 'v0', {verb : 'publish', item: JSON.stringify(item)}, 120);
+    return bc.invokeSmartContract(contx, 'drm', 'v0', {verb : 'changeCardLoanCredit', item: JSON.stringify(item)}, 120);
 };
 
 module.exports.end = function(results) {
@@ -51,17 +51,4 @@ module.exports.end = function(results) {
     }
     return Promise.resolve();
 };
-/**********************
-* save published items' identity
-**********************/
-/*var idfile = './tmp/ids.log'
-var fs = require('fs');
-module.exports.end = function(results) {
-     for (let i in results){
-        let stat = results[i];
-        if(stat.status === 'success') {
-            fs.appendFileSync(idfile, stat.result.toString() + '\n');
-        }
-    }
-    return Promise.resolve();
-}*/
+
